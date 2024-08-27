5c100df938faa459bc4fa519f924aa6a6f6bc49e
frostyfan109 <groupe@renci.org>
2024-08-27 15:42:32 -0400

    add manual grade endpoint

--- eduhelx_utils/api.py
+++ eduhelx_utils/api.py
@@ -8,12 +8,19 @@ import jwt
 import time
 import httpx
 from enum import Enum
+from typing import TypedDict
 from ._version import __version__
 
 class AuthType(Enum):
     APPSTORE_INSTRUCTOR = "appstore:instructor"
     APPSTORE_STUDENT = "appstore:student"
     PASSWORD = "password"
+
+class ManualGrade(TypedDict):
+    submission_id: int
+    # Between [0,1]
+    grade_proportion: float
+    comments: str | None
 
 class APIException(Exception):
     def __init__(self, response, message):
@@ -238,6 +245,9 @@ class Api:
             "master_notebook_content": master_notebook_content,
             "otter_config_content": otter_config_content
         })
+    
+    async def grade_assignment_manual(self, name: str, grade_data: list[ManualGrade]):
+        return await self._post(f"assignments/{name}/grade_manual", json=grade_data)
     
     
     """ Users """
